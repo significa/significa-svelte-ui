550eb3304a8563b4b79612a156ec7a5d86ce1379
Rui Sousa <30603437+ItzaMi@users.noreply.github.com>
2022-11-14 19:30:31 +0000

    feat(tests): `button.spec.ts`

---
+++ tests/button.spec.ts
@@ -0,0 +1,20 @@
+import { describe, expect, it } from 'vitest';
+import { render, screen } from '@testing-library/svelte';
+
+import Button from '../src/lib/components/button.svelte';
+
+describe('Button', () => {
+	const label = 'Click me';
+
+	beforeEach(() => {
+		render(Button, { label: label });
+	});
+
+	it('Renders a button', () => {
+		expect(screen.getByRole('button')).toBeInTheDocument();
+	});
+
+	it('Renders a button with a label', () => {
+		expect(screen.getByText(label)).toBeInTheDocument();
+	});
+});
