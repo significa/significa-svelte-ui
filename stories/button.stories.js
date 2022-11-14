1bf00b9215a317df1a67ef228e73c1a5374c9b92
Rui Sousa <30603437+ItzaMi@users.noreply.github.com>
2022-11-14 19:48:51 +0000

    feat(stories): `button.stories.js`

---
+++ stories/button.stories.js
@@ -0,0 +1,19 @@
+import Button from '../src/lib/components/button.svelte';
+
+export default {
+	title: 'Button',
+	component: Button,
+	argTypes: {
+		label: { control: 'text' }
+	}
+};
+
+const Template = (args) => ({
+	Component: Button,
+	props: args
+});
+
+export const Default = Template.bind({});
+Default.args = {
+	label: 'Button'
+};
