b1e57a8e5299a601b38faafec433bf2b805743ee
Rui Sousa <30603437+ItzaMi@users.noreply.github.com>
2022-11-16 18:15:49 +0000

    feat(components): create `Link`

---
+++ src/lib/components/Link.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	export let href: string;
+	export let label: string;
+</script>
+
+<a {href}>{label}</a>
+
+<style lang="postcss">
+	a {
+		padding: var(--space-4px) 0px;
+
+		text-decoration: none;
+	}
+</style>
