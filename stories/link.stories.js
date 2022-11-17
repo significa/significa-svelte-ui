1781227f34c36b21c29fa2534b1ebc1054331505
Rui Sousa <30603437+ItzaMi@users.noreply.github.com>
2022-11-17 13:39:15 +0000

    feat(Link): `stories`

---
+++ stories/link.stories.js
@@ -0,0 +1,21 @@
+import Link from '../src/lib/components/Link.svelte';
+
+export default {
+	title: 'Link',
+	component: Link,
+	argTypes: {
+		label: { control: 'text' },
+		href: { control: 'text' }
+	}
+};
+
+const Template = (args) => ({
+	Component: Link,
+	props: args
+});
+
+export const Default = Template.bind({});
+Default.args = {
+	label: 'A link',
+	href: 'https://www.google.com'
+};
