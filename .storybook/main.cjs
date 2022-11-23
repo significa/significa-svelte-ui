4748284edc7f0d16463cb1503bdb82ca2ed186b5
Rui Sousa <30603437+ItzaMi@users.noreply.github.com>
2022-11-23 09:52:38 +0000

    feat(storybook): add `dark-mode` toggle

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -4,7 +4,8 @@ module.exports = {
 	addons: [
 		'@storybook/addon-links',
 		'@storybook/addon-essentials',
-		'@storybook/addon-interactions'
+		'@storybook/addon-interactions',
+		'storybook-addon-themes'
 	],
 	framework: {
 		name: '@storybook/svelte-vite',
