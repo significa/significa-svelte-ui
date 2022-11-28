d4e75bf10e3b88eff2b685fff3c4221be1705e8c
Rui Sousa <30603437+ItzaMi@users.noreply.github.com>
2022-11-28 17:21:42 +0000

    chore(icons): create `Icon` component that takes a prop and render proper `svg` out of it

---
+++ src/lib/components/Icon.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import arrowRight from './Icons/arrow-right.svg?raw';
+	import handbook from './Icons/handbook.svg?raw';
+	import loading from './Icons/loading.svg?raw';
+
+	const icons = {
+		['arrow-right']: arrowRight,
+		handbook,
+		loading
+	};
+
+	export let icon: keyof typeof icons;
+</script>
+
+<i data-icon={icon}>
+	{@html icons[icon]}
+</i>
