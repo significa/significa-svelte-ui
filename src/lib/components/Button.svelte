e8b2c495a2b75c4ec1241ddf525837896c32bc07
Rui Sousa <30603437+ItzaMi@users.noreply.github.com>
2022-11-30 11:46:29 +0000

    fix(Button): transitions and hover effects

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -66,6 +66,16 @@
 
 		white-space: nowrap;
 
+		transition: box-shadow var(--transition-appearance), border var(--transition-appearance);
+
+		box-shadow: 0px 0px 0px 0px hsl(var(--color-accent-hsl) / var(--opacity-60));
+
+		@media (hover: hover) {
+			&:hover {
+				box-shadow: 0px 0px 0px 4px hsl(var(--color-accent-hsl) / var(--opacity-60));
+			}
+		}
+
 		&.solid {
 			background-color: var(--color-foreground);
 
@@ -82,22 +92,12 @@
 
 				color: var(--color-secondary);
 			}
-
-			@media (hover: hover) {
-				&:hover {
-					box-shadow: 0px 0px 0px 4px hsl(var(--color-accent-hsl) / var(--opacity-60));
-				}
-			}
 		}
 
 		&.outline {
 			border: 1px solid var(--color-smooth);
 
-			box-shadow: 0px 0px 0px 0px hsl(var(--color-accent-hsl) / var(--opacity-60));
-
 			color: var(--color-foreground);
-
-			transition: box-shadow var(--transition-motion);
 
 			&.loading {
 				color: hsl(var(--color-foreground-hsl) / var(--opacity-0));
@@ -109,7 +109,7 @@
 
 			@media (hover: hover) {
 				&:hover {
-					box-shadow: 0px 0px 0px 4px hsl(var(--color-accent-hsl) / var(--opacity-60));
+					border: 1px solid var(--color-accent);
 				}
 			}
 		}
