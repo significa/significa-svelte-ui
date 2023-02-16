07fe67e968cf4bec5bdb92c34cd1b10ff0a57c69
Pedro Brandão <pedro@significa.co>
2023-02-16 15:00:08 +0000

    chore: cleanup

--- stories/components/tag.story.svelte
+++
@@ -1,9 +0,0 @@
-<!-- Based on 
-https://github.com/cerebrl/forgerock-web-login-framework/pull/3/files#diff-8317ec6b5e00bb1202c179aeb2b4fe2e8e29e586e898dafee6da8a6a54af746e -->
-<script lang="ts">
-	import Tag from '../../src/lib/components/Tag.svelte';
-
-	export let href: string;
-</script>
-
-<Tag {href} {...$$restProps}>A link</Tag>
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,9 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import type { UserConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 
-const config: UserConfig = {
-	plugins: [sveltekit()],
-	test: {
-		globals: true,
-		environment: 'jsdom',
-		setupFiles: './setupTests.ts'
-	}
-};
-
-export default config;
+export default defineConfig({
+  plugins: [sveltekit()],
+  test: {
+    include: ['src/**/*.{test,spec}.{js,ts}']
+  }
+});
