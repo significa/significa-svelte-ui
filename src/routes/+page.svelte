55164980f5437b79fd22758d77a3339bda6844d3
Pedro Brandão <pedro@significa.co>
2023-02-23 15:08:20 +0000

    chore: clean home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,44 +1,5 @@
 <script lang="ts">
-  import Button from '$lib/components/button.svelte';
   import '../lib/styles/index.css';
-
-  let loading = false;
 </script>
 
 <h1>Significa UI</h1>
-<div data-theme="light">
-  <Button variant="primary" size="md" icon="handbook" arrow {loading}>Hello world</Button>
-  <Button variant="secondary" size="md" icon="handbook" arrow {loading}>Hello world</Button>
-  <Button variant="primary" size="md" arrow {loading}>Hello world</Button>
-  <Button variant="secondary" size="md" arrow {loading}>Hello world</Button>
-  <Button variant="primary" size="md" icon="handbook" {loading}>Hello world</Button>
-  <Button variant="secondary" size="md" icon="handbook" {loading}>Hello world</Button>
-</div>
-<div data-theme="dark">
-  <Button variant="primary" size="md" icon="handbook" arrow {loading}>Hello world</Button>
-  <Button variant="secondary" size="md" icon="handbook" arrow {loading}>Hello world</Button>
-  <Button variant="primary" size="md" arrow {loading}>Hello world</Button>
-  <Button variant="secondary" size="md" arrow {loading}>Hello world</Button>
-  <Button variant="primary" size="md" icon="handbook" {loading}>Hello world</Button>
-  <Button variant="secondary" size="md" icon="handbook" {loading}>Hello world</Button>
-</div>
-<div data-theme="yellow">
-  <Button variant="primary" size="md" icon="handbook" arrow {loading}>Hello world</Button>
-  <Button variant="secondary" size="md" icon="handbook" arrow {loading}>Hello world</Button>
-  <Button variant="primary" size="md" arrow {loading}>Hello world</Button>
-  <Button variant="secondary" size="md" arrow {loading}>Hello world</Button>
-  <Button variant="primary" size="md" icon="handbook" {loading}>Hello world</Button>
-  <Button variant="secondary" size="md" icon="handbook" {loading}>Hello world</Button>
-</div>
-
-<style>
-  div {
-    padding: 20px;
-    display: flex;
-    gap: 10px;
-  }
-
-  :global(body) {
-    font-family: var(--font-family-sans);
-  }
-</style>
