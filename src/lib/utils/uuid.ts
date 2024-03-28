51ccc6d7a9e57d27b150d85dcda16e6d4340de6e
kaaps <joao.guimaraes.ribeiro@gmail.com>
2024-03-28 09:23:29 +0000

    fix: revert

--- src/lib/utils/uuid.ts
+++ src/lib/utils/uuid.ts
@@ -1,8 +1,15 @@
-export function getUUID() {
+function getFallbackUUID() {
   return '10000000-1000-4000-8000-100000000000'.replace(/[018]/g, (c: string) =>
     (
       parseInt(c) ^
       (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (parseInt(c) / 4)))
     ).toString(16)
   );
+}
+
+export function getUUID() {
+  if (!('randomUUID' in crypto)) {
+    return getFallbackUUID();
+  }
+  return crypto.randomUUID();
 }
